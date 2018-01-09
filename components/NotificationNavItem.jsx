53e8c64c8b6fdbbeb10603754a1cc6d96f6498ce
Joe Roddy <mrjoeroddy@gmail.com>
2018-01-09 11:57:08 -0500

    missed a file in last commit >.<

--- components/NotificationNavItem.jsx
+++ components/NotificationNavItem.jsx
@@ -1,64 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 import { observer } from "mobx-react";
+import { Link } from "react-router-dom";
 
+import Notification from "./Notification";
 import notificationStore from "../../../stores/NotificationStore";
 
-@observer
-export default class NotificationNavItem extends Component {
-  state = {};
+const NotificationNavItem = props => {
+  const notifications = notificationStore.notifications;
+  let notifKeys = notifications ? Object.keys(notifications) : [];
+  notifKeys = notifKeys.reverse().slice(0, 6); //render 6 most recent notifs
 
-  render() {
-    const notifications = notificationStore.notifications;
-    const notifKeys = notifications ? Object.keys(notifications) : [];
-
-    return (
-      <div className="NotificationNavItem uk-navbar-item">
-        <a
-          href="#"
-          data-uk-icon="bell"
+  return (
+    <div className="NotificationNavItem uk-navbar-item">
+      <span className="uk-inline">
+        <Icon
           className={
-            "uk-icon " + (notifKeys.length > 0 ? "uk-text-danger" : "uk-text")
+            "uk-position-center uk-icon " +
+            (notifKeys.length > 0 ? "uk-text-danger" : "uk-text")
           }
+          type="bell"
         />
-        <div uk-dropdown="mode: click">
-          <ul className="uk-nav uk-dropdown-nav">
-            <li className="uk-nav-header">Notifications</li>
-            {notifKeys.reverse().map((notifId, i) => {
+        {notifKeys.length > 0 && (
+          <span className="uk-badge badge-danger uk-position-bottom-left">
+            {notifKeys.length}
+          </span>
+        )}
+      </span>
+      <div uk-dropdown="mode: click">
+        <ul className="uk-nav uk-dropdown-nav">
+          <li className="uk-nav-header uk-margin-small-bottom">
+            Notifications
+          </li>
+          <ul className="uk-list uk-list-divider">
+            {notifKeys.map((notifId, i) => {
               const notif = notifications[notifId];
-              if (!notif) return;
-              const actions = notif.actions && Object.keys(notif.actions);
-              return (
-                <li key={i}>
-                  <div>{notif.body}</div>
-                  <div className="uk-margin-small-top">
-                    {actions &&
-                      actions.map((action, actionI) => {
-                        return (
-                          <button
-                            key={actionI}
-                            className="uk-button uk-button-default uk-button-small"
-                            onClick={e => {
-                              notificationStore.handleNotificationAction(
-                                notif,
-                                action
-                              );
-                            }}
-                          >
-                            {action}
-                          </button>
-                        );
-                      })}
-                  </div>
-                  {i < notifKeys.length - 1 && (
-                    <hr className="uk-divider-small" />
-                  )}
-                </li>
-              );
+              if (!notif) return <span />;
+              return <Notification key={i} notification={notif} />;
             })}
             {notifKeys.length <= 0 && <li>No new notifications</li>}
+            <div className="uk-margin-small-top">
+              <Link to="/notifications">View all notifications</Link>
+            </div>
           </ul>
-        </div>
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default observer(NotificationNavItem);
+
+const Icon = props => {
+  //eslint-disable-next-line
+  return <a uk-icon={"icon: " + props.type} {...props} />;
+};
