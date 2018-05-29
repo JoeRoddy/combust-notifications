43ed6d21037d11e7c81689e8c138a197f5e91d91
Joe Roddy <mrjoeroddy@gmail.com>
2018-05-28 21:24:06 -0400

    import firebase as modules

--- db/NotificationDb.js
+++ db/NotificationDb.js
@@ -1,4 +1,5 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/database";
 
 class NotificationDb {
   createNotification(notification, receiverUid, userId) {
